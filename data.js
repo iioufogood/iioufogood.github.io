d670599705d6e3004aaca849d98928d8d65a3e08
iiiii <31815016+iioufogood@users.noreply.github.com>
2019-07-25 14:31:11 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,19 +3,54 @@ var stackCloudListData = [
         businessName: "多云统一管理",
         children: [
             {
-                name: "丰富的云服务目录",
+                name: "js文件下载",
                 detailOption: [
                     {
-                        name: "业务痛点1",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
+                        name: "1.通过a标签的方式来下载",
+                        description: "<a href='' target='_blank'></a>"
                     },
                     {
-                        name: "业务痛点2",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
+                        name: "2.通过提交form表单的方式",
+                        description:
+`var $form = $('<form action="'+url+'" method="post" target="rfFrame"></form>');
+angular.forEach(d,function(v,k){
+    v = JSON.stringify(v);
+    var $input = $('<input type="hidden" name="'+k+'"/>');
+    $input.attr('value',v);
+    $form.append($input);
+});
+$element.append($form);
+$form.submit();
+$form.remove();`
                     },                    
                     {
-                        name: "业务痛点3",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
+                        name: "3.通过http的请求拿到文件流的方式",
+                        description: 
+`$http({
+    url : $scope.url,
+    method : 'post',
+    data : $scope.form.formdata,
+    responseType : 'arraybuffer',
+}).then(function(d){
+    var data = d.data;
+        var blob = new Blob([data], {type: "text/plain"});
+       //var blob = new Blob([data], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});//xlsx
+       //var blob = new Blob([data], {type: "application/vnd.ms-excel"});//xls
+        var downloadTitle = '';
+        if(d.headers('content-disposition').indexOf('filename') != -1){
+            downloadTitle = d.headers('content-disposition').split(';')[1].split('=')[1];
+            downloadTitle = decodeURI(downloadTitle);
+        }
+        if(typeof window.navigator.msSaveBlob !== 'undefined'){
+            window.navigator.msSaveBlob(blob, downloadTitle);
+        }else{
+            var objectUrl = URL.createObjectURL(blob);
+            var aForExcel = $("<a download='"+downloadTitle+"'><span class='forExcel'></span></a>").attr("href",objectUrl);
+            $("body").append(aForExcel);
+            $(".forExcel").click();
+            aForExcel.remove();
+        }
+});`
                     },
                 ]
                 
@@ -118,176 +153,8 @@ var stackCloudListData = [
                         description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
                     }
                 }
-            },
-            {
-                name: "云联邦",
-                detailOption: {
-                    section1: {
-                        name: "一云多池5",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署" ,
-                        btnOptions: [
-                            {
-                                name: "立即体验",
-                                url: "http://www.baidu.com"
-                            }, {
-                                name: "Follow Me",
-                                url: "http://www.baidu.com"
-                            }
-                        ]                      
-                    },
-                    section2: {
-                        name: "业务痛点5",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    },
-                    section3: {
-                        name: "方案优势5",
-                        swiperOptions: [
-                            {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }
-                        ]
-                    },
-                    section4: {
-                        name: "业务价值5",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    }
-                }
-            }                                                
-        ]
-    },
-    {
-        businessName: "核心业务上云",
-        children: [
-            {
-                name: " 裸金属服务",
-                detailOption: {
-                    section1: {
-                        name: "一云多池",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署" ,
-                        btnOptions: [
-                            {
-                                name: "立即体验",
-                                url: "http://www.baidu.com"
-                            }, {
-                                name: "Follow Me",
-                                url: "http://www.baidu.com"
-                            }
-                        ]                      
-                    },
-                    section2: {
-                        name: "业务痛点",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    },
-                    section3: {
-                        name: "方案优势",
-                        swiperOptions: [
-                            {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }
-                        ]
-                    },
-                    section4: {
-                        name: "业务价值",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    }
-                }
-            },
-            {
-                name: "安全服务",
-                detailOption: {
-                    section1: {
-                        name: "一云多池",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署" ,
-                        btnOptions: [
-                            {
-                                name: "立即体验",
-                                url: "http://www.baidu.com"
-                            }, {
-                                name: "Follow Me",
-                                url: "http://www.baidu.com"
-                            }
-                        ]                      
-                    },
-                    section2: {
-                        name: "业务痛点",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    },
-                    section3: {
-                        name: "方案优势",
-                        swiperOptions: [
-                            {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }
-                        ]
-                    },
-                    section4: {
-                        name: "业务价值",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    }
-                }
-            },
-            {
-                name: "灾备服务",
-                detailOption: {
-                    section1: {
-                        name: "一云多池",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署" ,
-                        btnOptions: [
-                            {
-                                name: "立即体验",
-                                url: "http://www.baidu.com"
-                            }, {
-                                name: "Follow Me",
-                                url: "http://www.baidu.com"
-                            }
-                        ]                      
-                    },
-                    section2: {
-                        name: "业务痛点",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    },
-                    section3: {
-                        name: "方案优势",
-                        swiperOptions: [
-                            {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }, {
-                                imgUrl: "http://www.baidu.com",
-                                description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                            }
-                        ]
-                    },
-                    section4: {
-                        name: "业务价值",
-                        description: "大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署,大型企业区域分布广泛，会在多个地域同时开展业务，核心业务在私有云部署，同时会协同公有云资源，如何统一管理部署"
-                    }
-                }
-            }                                                
+            }                                               
         ]
     }
 ]
+
