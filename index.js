5c482c85abb79cf6a7c22b1d410fe8a419f87197
iiiii <31815016+iioufogood@users.noreply.github.com>
2019-07-25 16:45:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-//切换二级页面
-var onOff = true;
-function changeLevelTwoPage (onOff) {
-    //打开二级页面
-    onOff && $(".level-two-left").addClass("recovery");
-    onOff && $(".level-two-right").addClass("recovery");
-    onOff && $("#levelTwoContent").addClass("Zindex");
-    //关闭二级页面
-    !onOff && $(".level-two-left").removeClass("recovery");
-    !onOff && $(".level-two-right").removeClass("recovery");
-    !onOff && $("#levelTwoContent").removeClass("Zindex");
-}
+var baseUrl = "./data";
+var markNum = 1;
 
-// setTimeout(function () {
-//     changeLevelTwoPage(true);
-// }, 2000)
-var stackCloudList = stackCloudListData;
-
-var swiper = null;
-
+queryJson();
+var stackCloudList = [];
 var vueApp = new Vue({
   el: '#levelTwoContent',
   data: function () {
@@ -26,49 +11,74 @@ var vueApp = new Vue({
         stackCloudList: stackCloudList,
         targetBusiness: {},
         activeBusinessName: "",
-        activeChildName: ""
+        activeChildName: "",
+        childIndex: 0
     }
   },
   mounted: function () {
-    this.targetBusiness = stackCloudList[0].children[0];
-    this.activeBusinessName = stackCloudList[0].businessName;
-    this.activeChildName = stackCloudList[0].children[0].name;
+    // this.targetBusiness = stackCloudList[0].children[0];
+    // this.activeBusinessName = stackCloudList[0].businessName;
+    // this.activeChildName = stackCloudList[0].children[0].name;
   },
   methods: {
     goBackFn () {
         changeLevelTwoPage();
     },
-    businessShowFn (businessName, childName) {
+    businessShowFn (businessName, childName, index) {
         this.targetBusiness = this.stackCloudList.find(item => {
             return item.businessName === businessName
-        }).children.find(child => {
-            return child.name === childName;
+        }).children.find((child, ind) => {
+            return child.name === childName && ind === index;
         })
         this.activeBusinessName = businessName;
         this.activeChildName = childName;
+        this.childIndex = index;
     }
   },
   watch: {
     targetBusiness: function (val, oldVal) {
- 
+
     }
   }
 })
 
-$(document).on("click", ".animation-trigger a", function (e) {
-    var e = e || event;
-    var $target = $(e.target).is("a") ? $(e.target) : $(e.target).parents("a");
-    var dataForArr = $target.attr("data-for").split("|");
-    //展示二级页面
-    changeLevelTwoPage(true);
-    //初始化vue实例的数据
-    var targetBusiness = stackCloudList.find(item => {
-            return item.businessName === dataForArr[0]
-    }).children.find(child => {
-            return child.name === dataForArr[1];
+function queryJson () {
+    var url = baseUrl + markNum + ".json"
+    $.ajax({
+        "type": "GET",
+        "url": url,
+        "success": function (res) {
+            markNum++;
+            addData(res);
+            //递归
+            queryJson();
+        },
+        "error": function (e) {
+            console.log("请求失败!  " + "status:" + e.status, "请求失败url：" + url);
+            console.log(e);
+            Vue.set(vueApp, "stackCloudList", stackCloudList);
+            Vue.set(vueApp, "targetBusiness", stackCloudList[0].children[0]);
+            Vue.set(vueApp, "activeBusinessName", stackCloudList[0].businessName);
+            Vue.set(vueApp, "activeChildName", stackCloudList[0].children[0].name);          
+        }
     })
-    Vue.set(vueApp, "activeBusinessName", dataForArr[0]);
-    Vue.set(vueApp, "activeChildName", dataForArr[1]);
-    Vue.set(vueApp, "targetBusiness", targetBusiness);
+}
 
-})
+function addData (data) {
+    if (stackCloudList.length === 0) {
+        stackCloudList = data;
+    } else {
+        data.forEach(function (item) {
+            var hasBusiness = false;
+            stackCloudList.forEach(function (stackItem) {
+                if (stackItem.businessName === item.businessName) {
+                    stackItem.children = [...stackItem.children, ...item.children];
+                    hasBusiness = true;
+                }
+            })
+            if (!hasBusiness) {
+                stackCloudList = [...stackCloudList, item];
+            }
+        })
+    }
+}
