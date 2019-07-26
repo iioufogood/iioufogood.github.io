b9da807da95766a1a4b6e3b22bf8fa320c161361
iiiii <31815016+iioufogood@users.noreply.github.com>
2019-07-26 16:35:41 +0800

    Update data2.js

--- data/data2.js
+++ data/data2.js
@@ -1,23 +1,22 @@
 $(function(){
     var data = [
         {
-            "businessName": "javascript相关test",
+            "businessName": "常用js插件",
             "children": [
                 {
-                    "name": "js文件下载",
+                    "name": "lottie动画",
                     "detailOption": [
                         {
-                            "name": "1.通过a标签的方式来下载",
+                            "name": "引入库文件",
                             "description": "&lt;a href='' target='_blank'&gt;&lt;/a&gt;<br>"
                         },
                         {
-                            "name": "2.通过提交form表单的方式",
-                            "description":"<div>var $form = $(\"&lt;form action='\"+url+\"' method='post' target='rfFrame'&gt;&lt;/form&gt;\");</div><div>angular.forEach(d,function(v,k){</div><div>&nbsp; &nbsp; v = JSON.stringify(v);</div><div>&nbsp; &nbsp; var $input = $(\"&lt;input type='hidden' name='\"+k+\"'/&gt;\");</div><div>&nbsp; &nbsp; $input.attr(\"value\",v);</div><div>&nbsp; &nbsp; $form.append($input);</div><div>});</div><div>$element.append($form);</div><div>$form.submit();</div><div>$form.remove();</div>"
+                            "name": "提取动画数据",
+                            "description":"<div>&nbsp; &nbsp; let animationData = {......};</div><div>&nbsp; &nbsp; let params = {</div><div>&nbsp; &nbsp; &nbsp; &nbsp; container: document.getElementById(\"lottie\"),</div><div>&nbsp; &nbsp; &nbsp; &nbsp; renderer: \"canvas\",</div><div>&nbsp; &nbsp; &nbsp; &nbsp; loop: true,</div><div>&nbsp; &nbsp; &nbsp; &nbsp; autoplay: true,</div><div>&nbsp; &nbsp; &nbsp; &nbsp; path: \"data.json\"</div><div>&nbsp; &nbsp; }</div><div>&nbsp; &nbsp; let anim = lottie.loadAnimation(params);</div>"
                         },
                         {
-                            "name": "3.通过http的请求拿到文件流的方式",
-                            "description": "<div>$http({</div><div>&nbsp; &nbsp; url : $scope.url,</div><div>&nbsp; &nbsp; method : \"post\",</div><div>&nbsp; &nbsp; data : $scope.form.formdata,</div><div>&nbsp; &nbsp; responseType : \"arraybuffer\",</div><div>}).then(function(d){</div><div>&nbsp; &nbsp; var data = d.data;</div><div>&nbsp; &nbsp; &nbsp; &nbsp; var blob = new Blob([data], {type: 'text/plain'});</div><div>&nbsp; &nbsp; &nbsp; &nbsp;//var blob = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});//xlsx</div><div>&nbsp; &nbsp; &nbsp; &nbsp;//var blob = new Blob([data], {type: 'application/vnd.ms-excel'});//xls</div><div>&nbsp; &nbsp; &nbsp; &nbsp; var downloadTitle = \"\";</div><div>&nbsp; &nbsp; &nbsp; &nbsp; if(d.headers(\"content-disposition\").indexOf(\"filename\") != -1){</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; downloadTitle = d.headers(\"content-disposition\").split(\";\")[1].split(\"=\")[1];</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; downloadTitle = decodeURI(downloadTitle);</div><div>&nbsp; &nbsp; &nbsp; &nbsp; }</div><div>&nbsp; &nbsp; &nbsp; &nbsp; if(typeof window.navigator.msSaveBlob !== \"undefined\"){</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; window.navigator.msSaveBlob(blob, downloadTitle);</div><div>&nbsp; &nbsp; &nbsp; &nbsp; }else{</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; var objectUrl = URL.createObjectURL(blob);</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; var aForExcel = $('&lt;a download=\"'+downloadTitle+'\"&gt;&lt;span class=\"forExcel\"&gt;&lt;/span&gt;&lt;/a&gt;').attr('href',objectUrl);</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; $('body').append(aForExcel);</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; $('.forExcel').click();</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; aForExcel.remove();</div><div>&nbsp; &nbsp; &nbsp; &nbsp; }</div><div>});</div>"
-                        }
+                            "name": "配置项",
+                            "description": "<div>container：为预留的动画容器；</div><div><br></div><div>renderer：为动画模式，html为纯dom结构动画，canvas为画板动画，svg为svg动画；</div><div><br></div><div>loop：为循环</div><div><br></div><div>autoplay：为自动播放</div><div><br></div><div>animationData：为动画数据，该内容与data.js内容一致，所以data.js 可以不使用；</div><div><br></div><div>path：为动画数据，与animationData不可同时使用，数值为data.json的路径</div><div><br></div><div>常用方法：</div><div><br></div><div>输出动画时长秒：</div><div><br></div><div>console.log(\"时长(s):\" + anim.getDuration(false));</div><div>输出动画时长帧：</div><div><br></div><div>console.log(\"时长(帧):\" + anim.getDuration(true));</div><div>暂停：</div><div><br></div><div>anim.pause();</div><div>播放：</div><div><br></div><div>anim.play();</div><div>停止：</div><div><br></div><div>anim.stop();</div><div>销毁动画实例:</div><div><br></div><div>anim.destroy();</div><div>正/反向播放:</div><div><br></div><div>anim.setDirection(parm);</div><div>parm: 1为正向，-1为反向</div><div><br></div><div>播放速度：</div><div><br></div><div>anim.setSpeed(num);</div>"
                     ]
                 },
                 {
