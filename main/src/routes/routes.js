ad2e251426425432006aec1230346a12a21d1fda
iiiii <31815016+iioufogood@users.noreply.github.com>
2019-07-30 20:26:47 +0800

    Update routes.js

--- main/src/routes/routes.js
+++ main/src/routes/routes.js
@@ -1,8 +1,21 @@
-define(['src/views/login'], function (login) {
-    return [
-        {
-            path: '/login',
-            component: login
-        }
-    ]
+define(['src/views/login', 'src/views/home', 'src/views/home/transformstr'], function (login, home, transformstr) {
+	return [
+		{
+		    path: '/login',
+		    name: 'login',
+		    component: login,
+		 },
+		{
+		    path: '/home',
+		    name: 'home',
+		    component: home,
+		    children: [
+		    	{
+		    		path: '/home/transformstr',
+		    		name: 'transformstr',
+		    		component: transformstr
+		    	}
+		    ]
+		 } 		 
+	]
 })
